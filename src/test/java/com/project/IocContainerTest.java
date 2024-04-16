ede38d0dd75e17e91726f28201867207ebcc0bd9
Zhou Di <921748292@qq.com>
2024-04-16 22:57:33 +0800

    feat:通过Spring Framework来整合JUnit4。

--- src/test/java/com/project/IocContainerTest.java
+++ src/test/java/com/project/IocContainerTest.java
@@ -3,18 +3,28 @@ package com.project;
 import com.project.config.SpringConfig;
 import com.project.web.serlvet.UserServlet;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.persistence.EntityManagerFactory;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
 public class IocContainerTest {
+
+    // 注入ApplicationContext
+    @Autowired
+    private ApplicationContext applicationContext;
+
     /**
      * 此测试方法从容器中获取所有的bean并打印出来
      */
     @Test
     public void testIocContainer() {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
         String[] beanCollector = applicationContext.getBeanDefinitionNames();
         for (String bean : beanCollector) {
             System.out.println(bean);
@@ -26,7 +36,6 @@ public class IocContainerTest {
      */
     @Test
     public void testIocContainerForUserServlet() {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
         UserServlet userServlet = applicationContext.getBean(UserServlet.class);
         System.out.println(userServlet);
     }
